a463752a7d43e6f06eebc51290f7aa2405f55e45
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-08 15:49:23 -0600

    initialize

---
+++ ngHagane.js
@@ -0,0 +1,28 @@
+// 1. define the module and the other module dependencies (if any)
+var ngHagane = angular.module('ngHagane', []);
+ngHagane.constant('MODULE_VERSION', '0.0.1');
+// ngHagane.factory('factoryName', function() {/* stuff here */});
+// ngHagane.directive('directiveName', function() {/* stuff here */});
+
+// angular.module('ngHagane').controller('main', function() {
+// 	/* more stuff here */
+// });
+
+ngHagane.provider("hagane", function () {
+	this.server;
+	this.appToken;
+	this.accessToken;
+
+	this.setServer = function (server) {
+		this.server = server;
+	}
+
+	this.$get = function () {
+		var self = this;
+		return {
+			getServer: function () {
+				return self.server;
+			},
+		}
+	};
+});
