a7506927546f0df9cf3b5e8f1be43c048141c0da
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-28 20:38:22 -0600

    cambio de nombre

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var concat = require('gulp-concat');
 
 gulp.task('default', function() {
   gulp.src('src/**/*.js')
-    .pipe(concat('ngHagane.js'))
+    .pipe(concat('angular-hagane.js'))
     .pipe(gulp.dest('dist'));
 });
